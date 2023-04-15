a0aca94d0abb6a34a5906795d0f01647413c496e
waqar <wakku9780@gmail.com>
2023-04-16 01:54:31 +0530

    Assignment wh

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static class A {
+
+        public  String meth()
+        {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+        public  String meth()
+        {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.meth();
+
+        B obj1 = new B();
+        obj1.meth();
+    }
+
 }
