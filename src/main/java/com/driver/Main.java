94d6b9afb2ed4d7dbfc45ee60374132e2ca99ce6
waqar <wakku9780@gmail.com>
2023-04-16 02:06:11 +0530

    assignment wa

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,3 +1,4 @@
+
 package com.driver;
 
 public class Main {
@@ -17,7 +18,11 @@ public class Main {
         }
     }
     public static void main(String[] args) {
+        B obj = new B();
+        obj.meth();
 
-    }
+        B obj1 = new B();
+        obj1.meth();
+}
 
 }
